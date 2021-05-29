dc82c9961bf1aa9ce60c96c6653c08615b350168
anntians <anntians@usc.edu>
2021-05-29 11:01:01 +0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+/* Hello world example
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!'); //displays a pop-up box with the message
+}
+*/
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/me-small.jpg') {
+      myImage.setAttribute('src','images/me2.jpg');
+    } else {
+      myImage.setAttribute('src','images/me-small.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() { //setting username
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Kingmamba is smexy, ' + myName;
+    }
+  }
+
+  if(!localStorage.getItem('name')) { //if no username, then create one
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Kingmamba is smexy, ' + storedName;
+  }
+
+  myButton.onclick = function() { //click on website to switch username
+    setUserName();
+  }
